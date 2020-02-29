d169b29b1c76c24652603ad23820da5e86cf7f9c
Henry Zhang <henryzhang9802@gmail.com>
2020-02-29 15:06:36 +0000

    Add hit indicator

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-var audio = document.getElementById("audio-element");
+let audio = document.getElementById("audio-element");
 
-var canvas = document.querySelector("canvas");
+let canvas = document.querySelector("canvas");
 
-var ctx = this.canvas.getContext("2d");
+let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let arr = [];
+let dropTrackArr = [];
+
+let timeArr = [];
 console.log(audio.currentTime);
 
 window.onload = function() {
@@ -14,20 +16,48 @@ window.onload = function() {
     "keydown",
     function(event) {
       console.log(audio.currentTime, event.key);
-      arr.push({ time: audio.currentTime, key: event.key });
+      timeArr.push({ time: audio.currentTime, key: event.key });
+      for (track of dropTrackArr) {
+        track.keyDown(event.key);
+      }
     },
     false
   );
 };
+
+//hit indicator gradient
+let hitGradient = ctx.createLinearGradient(0, (canvas.height / 10) * 8, 0, canvas.height);
+hitGradient.addColorStop(0, "rgba(0,0,0,0)");
+hitGradient.addColorStop(1, "yellow");
 
 function dropTrack(x, width, keyBind) {
   this.x = x;
   this.width = width;
   this.keyBind = keyBind;
 
+  this.hitIndicatorOpacity = 0;
+
+  this.keyDown = function(key) {
+    if (keyBind == key) {
+      this.hitIndicatorOpacity = 1;
+    }
+  };
+
   this.update = function() {
+    //track bg
     ctx.fillStyle = "#212121";
     ctx.fillRect(x, 0, width, canvas.height);
+    //hit line
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(x, (canvas.height / 10) * 9, this.width, 10);
+    //hit indicator
+    if (this.hitIndicatorOpacity > 0) {
+      ctx.fillStyle = hitGradient;
+      ctx.globalAlpha = this.hitIndicatorOpacity;
+      ctx.fillRect(x, (canvas.height / 10) * 8, this.width, (canvas.height / 10) * 2);
+      this.hitIndicatorOpacity -= 0.01;
+      ctx.globalAlpha = 1;
+    }
   };
 }
 
@@ -35,7 +65,7 @@ let trackNum = 4;
 let trackWidth = 150;
 
 let startX = canvas.width / 2 - (trackNum * trackWidth) / 2;
-let dropTrackArr = [];
+
 dropTrackArr.push(new dropTrack(startX + trackWidth * 0, trackWidth, "d"));
 dropTrackArr.push(new dropTrack(startX + trackWidth * 1 + 2, trackWidth, "f"));
 dropTrackArr.push(new dropTrack(startX + trackWidth * 2 + 4, trackWidth, "j"));
@@ -43,6 +73,8 @@ dropTrackArr.push(new dropTrack(startX + trackWidth * 3 + 6, trackWidth, "k"));
 
 function animate() {
   requestAnimationFrame(animate);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (track of dropTrackArr) {
     track.update();
   }

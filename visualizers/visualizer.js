fd802f9041b398579db493cd142d24a0210c2326
Henry Zhang <henryzhang9802@gmail.com>
2020-03-01 17:53:29 +0000

    Fix visualizer

--- visualizers/visualizer.js
+++ visualizers/visualizer.js
@@ -1,59 +1,57 @@
-window.onload = function() {
-  audio.crossOrigin = "anonymous";
-  let context = new AudioContext();
-  let src = context.createMediaElementSource(audio);
-  let analyser = context.createAnalyser();
+audio.crossOrigin = "anonymous";
+let context = new AudioContext();
+let src = context.createMediaElementSource(audio);
+let analyser = context.createAnalyser();
 
-  let visualizerCanvas = document.getElementById("visualizer");
-  visualizerCanvas.width = window.innerWidth;
-  visualizerCanvas.height = window.innerHeight;
-  let vCtx = visualizerCanvas.getContext("2d");
+let visualizerCanvas = document.getElementById("visualizer");
+visualizerCanvas.width = window.innerWidth;
+visualizerCanvas.height = window.innerHeight;
+let vCtx = visualizerCanvas.getContext("2d");
 
-  src.connect(analyser);
-  analyser.connect(context.destination);
+src.connect(analyser);
+analyser.connect(context.destination);
 
-  analyser.fftSize = 256;
+analyser.fftSize = 256;
 
-  let bufferLength = analyser.frequencyBinCount;
+let bufferLength = analyser.frequencyBinCount;
 
-  let dataArray = new Uint8Array(bufferLength);
+let dataArray = new Uint8Array(bufferLength);
 
-  let WIDTH = visualizerCanvas.width;
-  let HEIGHT = visualizerCanvas.height;
+let WIDTH = visualizerCanvas.width;
+let HEIGHT = visualizerCanvas.height;
 
-  let barWidth = (WIDTH / bufferLength) * 2.5;
-  let barHeight;
-  let x = 0;
+let barWidth = (WIDTH / bufferLength) * 2.5;
+let barHeight;
+let x = 0;
 
-  function renderFrame() {
-    requestAnimationFrame(renderFrame);
+function renderFrame() {
+  requestAnimationFrame(renderFrame);
 
-    x = 0;
+  x = 0;
 
-    analyser.getByteFrequencyData(dataArray);
+  analyser.getByteFrequencyData(dataArray);
 
-    vCtx.fillStyle = "#000";
-    vCtx.fillRect(0, 0, WIDTH, HEIGHT);
+  vCtx.fillStyle = "#000";
+  vCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    for (let i = 0; i < bufferLength; i++) {
-      barHeight = dataArray[i] * 3;
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i] * 3;
 
-      let r = (x / WIDTH) * 255;
-      let g = 50 * (i / bufferLength);
-      let b = barHeight + 10 * (i / bufferLength);
+    let r = (x / WIDTH) * 255;
+    let g = 50 * (i / bufferLength);
+    let b = barHeight + 10 * (i / bufferLength);
 
-      let temp = 0;
-      const transValue = 100;
-      if (barHeight != 0) {
-        for (let j = 0; j <= barHeight; j += 2) {
-          temp += 1 / barHeight / 2;
-          vCtx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + temp + ")";
-          vCtx.fillRect(x, HEIGHT - barHeight + j + 10, barWidth, barHeight / transValue);
-        }
-        x += barWidth;
+    let temp = 0;
+    const transValue = 100;
+    if (barHeight != 0) {
+      for (let j = 0; j <= barHeight; j += 2) {
+        temp += 1 / barHeight / 2;
+        vCtx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + temp + ")";
+        vCtx.fillRect(x, HEIGHT - barHeight + j + 10, barWidth, barHeight / transValue);
       }
+      x += barWidth;
     }
   }
+}
 
-  renderFrame();
-};
+renderFrame();

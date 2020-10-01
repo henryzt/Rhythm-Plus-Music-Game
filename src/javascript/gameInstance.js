4a816fa8be225962dc05e8f76a3b91951a0cd2a7
Henry Zhang <henryzhang9802@gmail.com>
2020-10-01 12:06:37 +0100

    Revert gameloop function changes

--- src/javascript/gameInstance.js
+++ src/javascript/gameInstance.js
@@ -388,7 +388,7 @@ export default class GameInstance {
     if (shouldAdvance) this.timeArrIdx++;
     if (this.vm.perspective) this.drawFadeOut();
     this.countFps();
-    this.gameTimingLoop();
+    // this.gameTimingLoop();
   }
 
   countFps() {
@@ -439,7 +439,7 @@ export default class GameInstance {
         clearInterval(intervalPrePlay);
         clearInterval(this.intervalPlay);
         this.vm.started = true;
-        // this.intervalPlay = setInterval(this.gameTimingLoop.bind(this), 100);
+        this.intervalPlay = setInterval(this.gameTimingLoop.bind(this), 100);
       }
     }, 100);
   }

c6ddf3cc7aba77f8135bbe117e9b3258779454e0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-05 13:42:13 +0000

    Deploy henryz00/Rhythm-Plus-Music-Game to github.com/henryz00/Rhythm-Plus-Music-Game.git:gh-pages

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const FILES_TO_CACHE = ["/offline.html", "/style.css", "/assets/logo2.png"];
+const CACHE_NAME = "rhythm_plus";
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Pre-caching offline page");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.mode !== "navigate") {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match("offline.html");
+      });
+    })
+  );
+});

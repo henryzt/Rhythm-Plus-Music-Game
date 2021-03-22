eea9d6fe8df9473ea7e269de435c96e06af3233a
Henry Zhang <henryzhang9802@gmail.com>
2021-03-22 21:00:24 +0000

    Update webpack caching and bump sass

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   resolve: { extensions: ["*", ".js"] },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].bundle.js",
     publicPath: "/",
   },
   devServer: {
